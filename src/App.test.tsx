d0a9f70f5f590d1d3f360bfa254e54880fcf24f4
Mayank Singh Rathore <mayank.rathore@admiralindia.com>
2024-09-14 22:45:20 +0530

    first commit

--- src/App.test.tsx
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// import App from "./App";
-// import { render, screen } from '@testing-library/react';
-// test('Testing ', () => { })
--- src/components/Create.test.tsx
+++
@@ -1,8 +0,0 @@
-import Create from "./Create";
-import { render,screen } from "@testing-library/react";
-test('checking input',
-    () => { 
-        render(<Create/>)
-        let checkInput=screen.getByRole("textbox")
-        expect(checkInput).toBeInTheDocument();
-     })
